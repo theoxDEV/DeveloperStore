d8cbf8e8f40ce9a8be7ecfc2c63a19ba50d62574
Matheus Nascimento <56891192+theoxDEV@users.noreply.github.com>
2025-04-11 14:43:18 -0300

    Initial commit of tech test without repository

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.ToTable("Sales");
+
+            builder.HasKey(sale => sale.Id);
+
+            builder.Property(sale => sale.SaleNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(sale => sale.SaleDate)
+                .IsRequired();
+
+            builder.Property(sale => sale.CustomerId)
+                .IsRequired();
+
+            builder.Property(sale => sale.CustomerName)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(sale => sale.BranchId)
+                .IsRequired();
+
+            builder.Property(sale => sale.BranchName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(sale => sale.TotalAmount)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(sale => sale.IsCancelled)
+                .IsRequired();
+
+            builder.HasMany(sale => sale.Items)
+                .WithOne(item => item.Sale)
+                .HasForeignKey(item => item.SaleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable("SaleItems");
+
+            builder.HasKey(item => item.Id);
+
+            builder.Property(item => item.ProductId)
+                .IsRequired();
+
+            builder.Property(item => item.ProductName)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(item => item.Quantity)
+                .IsRequired();
+
+            builder.Property(item => item.UnitPrice)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(item => item.Discount)
+                .HasColumnType("decimal(5,2)")
+                .IsRequired();
+
+            builder.Property(item => item.IsCancelled)
+                .IsRequired();
+
+            builder.Property(item => item.SaleId)
+                .IsRequired();
+
+            builder.HasOne(item => item.Sale)
+                .WithMany(sale => sale.Items)
+                .HasForeignKey(item => item.SaleId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
